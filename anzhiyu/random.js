68a80a56ee9f7936bdda730d4143c12bac6bf8ce
likeyu88 <974972732@qq.com~>
2024-08-06 18:27:46 +0800

    Site updated: 2024-08-06 18:27:46

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/08/02/第二篇博文/","2024/08/02/李可喻的博客/","2024/08/02/hello-world/","2024/05/03/新的/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
